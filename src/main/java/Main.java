ffd1767ab70ff56956a32328976d38c7e51ee4db
nubk <nicole.ubk@outlook.com>
2017-07-25 18:46:34 +0200

    Add echo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,10 @@ import io.javalin.Javalin;
 
 public class Main {
     public static void main(String[] args) {
-        Javalin app = Javalin.create().port(7000);
-        app.get("/", ctx -> ctx.result("Hello World"));
+        Javalin app = Javalin.create();
+        app.port(7000);
+        app.enableStaticFiles("/public");
+        app.start();
+        app.post("/run-code", ctx -> ctx.result(ctx.formParam("code")));
     }
 }
