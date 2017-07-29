851d00ee94452fe3fbf65f65754052064b10ca32
nubk <nicole.ubk@outlook.com>
2017-07-29 15:41:08 +0200

    Make "run code" skeleton

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,31 @@
 import io.javalin.Javalin;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        Javalin app = Javalin.create();
-        app.port(7000);
-        app.enableStaticFiles("/public");
-        app.start();
-        app.post("/run-code", ctx -> ctx.result(ctx.formParam("code")));
+
+        Javalin app = Javalin.create()
+            .port(7000)
+            .enableStaticFiles("/public")
+            .start();
+
+        app.get("/run-code", ctx -> {
+            ctx.renderVelocity("/velocity/code-editor.vm", new HashMap<>());
+        });
+
+        app.post("/run-code", ctx -> {
+            String input = ctx.formParam("code");
+            String output = runCode(input);
+            Map<String, Object> model = new HashMap<>();
+            model.put("codeInput", input);
+            model.put("codeOutput", output);
+            ctx.renderVelocity("/velocity/code-editor.vm", model);
+        });
+    }
+
+    public static String runCode(String input){
+        return new StringBuilder(input).reverse().toString();
     }
 }
