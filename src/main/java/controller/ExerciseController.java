c92c1f315238790ca669411a61cf07d0893f87f8
nubk <nicole.ubk@outlook.com>
2017-11-02 21:50:38 +0100

    Add more exercises

--- src/main/java/controller/ExerciseController.java
+++ src/main/java/controller/ExerciseController.java
@@ -16,7 +16,19 @@ public class ExerciseController {
                     Language.JAVASCRIPT, "helloWorld() === 'Hello, World!'",
                     Language.PYTHON, "helloWorld() == 'Hello, World!'"
             )),
-            new Exercise("Exercise 2", "Filter array", "Create a method called filterEven(array) which filters out odd numbers from an array of ints", ImmutableMap.of(
+            new Exercise("Exercise 2", "Control Flow: if/else statements", "Create a method 'isGreaterThanTen(number)' that returns OK if number is greater than 10, and KO if number is less than 10.", ImmutableMap.of(
+                    Language.JAVASCRIPT, "isGreaterThanTen(5) === 'KO' && isGreaterThanTen(555) === 'OK'",
+                    Language.PYTHON, "isGreaterThanTen(5) == 'KO' and isGreaterThanTen(555) == 'OK'"
+            )),
+            new Exercise("Exercise 3", "Control Flow: switch", "Create a method called seasonName(number) that takes a number from 1 to 4, and returns 'Spring', 'Summer', 'Fall', 'Winter' respectively.", ImmutableMap.of(
+                    Language.JAVASCRIPT, "seasonName(1) === 'Spring' && seasonName(2) === 'Summer' && seasonName(3) === 'Fall' && seasonName(4) === 'Winter'",
+                    Language.PYTHON, "seasonName(1) == 'Spring' and seasonName(2) == 'Summer' and seasonName(3) == 'Fall' and seasonName(4) == 'Winter'"
+            )),
+            new Exercise("Exercise 4", "For-loop", "Create a method called summation(number) that adds numbers 0 through number (0 + 1 + ... + number)", ImmutableMap.of(
+                    Language.JAVASCRIPT, "summation(5) === 5+4+3+2+1+0 && summation(100) === 5050",
+                    Language.PYTHON, "summation(5) == 5+4+3+2+1+0 and summation(100) == 5050"
+            )),
+            new Exercise("Exercise 5", "Filter array", "Create a method called filterEven(array) which filters out odd numbers from an array of ints", ImmutableMap.of(
                     Language.JAVASCRIPT, "filterEven([1,2,3,4,5,6,7,8,9,10]).toString() === [2,4,6,8,10].toString()",
                     Language.PYTHON, "filterEven([1,2,3,4,5,6,7,8,9,10]) == [2, 4, 6, 8, 10]"
             ))
